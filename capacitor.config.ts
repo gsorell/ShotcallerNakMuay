1eb1694ed1af598d847691f60f1c25100a7acb7e
Or Ricon <rikonor@gmail.com>
2025-12-04 12:55:24 -0500

    move contents of client dir up and remove client dir

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.shotcallernakmuay.app",
+  appName: "Shotcaller Nak Muay",
+  webDir: "dist",
+};
+
+export default config;
--- client/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.shotcallernakmuay.app',
-  appName: 'Shotcaller Nak Muay',
-  webDir: 'dist'
-};
-
-export default config;
